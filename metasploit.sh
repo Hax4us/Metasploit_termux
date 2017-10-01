bd8279696deb73e5df6a8cfe2b3152604fd77910
Hax4us <30441647+Hax4us@users.noreply.github.com>
2017-10-01 17:45:00 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -20,7 +20,7 @@ cd $HOME/metasploit-framework
 sed '/rbnacl/d' -i Gemfile.lock
 sed '/rbnacl/d' -i metasploit-framework.gemspec
 gem install bundler
-
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
 
 gem install nokogiri -v 1.8.0 -- --use-system-libraries
  
