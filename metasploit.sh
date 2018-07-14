a171b1dfc3744b1eeef617bb1136f2b696f02ffe
Hax4us <30441647+Hax4us@users.noreply.github.com>
2018-07-14 06:06:20 +0000

    Update

--- metasploit.sh
+++ metasploit.sh
@@ -1,66 +1,64 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "##############################################"
-echo " I  AM  DEVIL (Lokesh) , PLZ SUBSCRIBE MY CHANNEL"
-echo "##############################################"
 
-echo "WAIT UNTIL INSTALLING............" 
-
-echo "####################################"
-apt install -y autoconf bison clang coreutils axel curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
-echo "####################################"
-apt update && apt upgrade
-echo "Downloading & Extracting....."
-
-# Remove Old Tars
-find $HOME -name "*.16.tar.gz" -type f -delete
-
-# Remove Old Folders
+# Remove Old Tars if exist
+find $HOME -name "*17.1.tar.gz" -type f -delete
+# Remove  Old Folder if exist 
 find $HOME -name "metasploit*" -type d -delete
-
-cd $HOME
-axel --alternate https://github.com/rapid7/metasploit-framework/archive/4.16.16.tar.gz
-tar -xf $HOME/metasploit-framework-4.16.16.tar.gz
-mv $HOME/metasploit-framework-4.16.16 $HOME/metasploit-framework
-rm $HOME/metasploit-framework-4.16.16.tar.gz
-cd $HOME/metasploit-framework
-sed '/rbnacl/d' -i Gemfile.lock
-sed '/rbnacl/d' -i metasploit-framework.gemspec
-gem install bundler
-sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
 # Many phones are claiming libxml2 not found error
 ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
 
-gem install nokogiri -v 1.8.0 -- --use-system-libraries
- 
-#sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock
-#gem unpack grpc -v 1.4.1
-#cd grpc-1.4.1
-#curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
-#curl -L https://raw.githubusercontent.com/Hax4us/Hax4us.github.io/master/extconf.patch
-#patch -p1 < extconf.patch
-#gem build grpc.gemspec
-#gem install grpc-1.4.1.gem
-#cd ..
-#rm -r grpc-1.4.1
 
+cwd=$(pwd)
+msfvar=4.17.1
+msfpath='/data/data/com.termux/files/home'
 
-cd $HOME/metasploit-framework
+apt update
+apt install -y autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
+
+cd $msfpath
+curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
+
+tar -xf $msfpath/$msfvar.tar.gz
+mv $msfpath/metasploit-framework-$msfvar $msfpath/metasploit-framework
+cd $msfpath/metasploit-framework
+sed '/rbnacl/d' -i Gemfile.lock
+sed '/rbnacl/d' -i metasploit-framework.gemspec
+gem install bundler
+
+isNokogiri=$(gem list nokogiri -i)
+
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+if [ $isNokogiri == "false" ];
+then
+      gem install nokogiri -v '1.8.0' -- --use-system-libraries
+else
+	echo "nokogiri already installed"
+fi
+
+cd $msfpath/metasploit-framework
 bundle install -j5
+
+echo "Gems installed"
 $PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
 rm ./modules/auxiliary/gather/http_pdf_authors.rb
-if [ -e $PREFIX/bin/msfconsole ];then
-	rm $PREFIX/bin/msfconsole
+
+if [ -e $PATH/bin/msfconsole ];then
+	rm $PATH/bin/msfconsole
 fi
-if [ -e $PREFIX/bin/msfvenom ];then
-	rm $PREFIX/bin/msfvenom
+if [ -e $PATH/bin/msfvenom ];then
+	rm $PATH/bin/msfvenom
 fi
-ln -sf $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
-ln -sf $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+ln -s $msfpath/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+ln -s $msfpath/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+
 termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
 echo "Creating database"
 
-cd $HOME/metasploit-framework/config
+cd $msfpath/metasploit-framework/config
+
 curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/database.yml
+
 
 mkdir -p $PREFIX/var/lib/postgresql
 initdb $PREFIX/var/lib/postgresql
@@ -69,16 +67,6 @@ pg_ctl -D $PREFIX/var/lib/postgresql start
 createuser msf
 createdb msf_database
 
-echo "###############################"
-echo "Thanx  To  Vishalbiswani"
-echo "###############################"
+rm $msfpath/$msfvar.tar.gz
 
-echo "###############################################"
-echo "Hello  I  Am  Devil  (Lokesh) ,  Subscribe  My  Channel  To  Motivate  My  Work"
-echo "###############################################"
-echo "###############################"
-echo "For  More. Tricks. Visit  At  hax4us.com"
-echo "###############################"
-echo "####################################"
-echo " NOW YOU CAN LAUNCH METASPLOIT BY JUST EXECUTE THE COMMAND :=> msfconsole"
-echo "####################################"
+echo "you can directly use msfvenom or msfconsole rather than ./msfvenom or ./msfconsole."
