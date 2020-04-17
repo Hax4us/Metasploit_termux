6d02dec85e80c705958b2b1bfe092bcf9b83c7e1
Lokesh Pandey <30441647+Hax4us@users.noreply.github.com>
2020-04-17 08:56:17 +0000

    Update msf

--- metasploit.sh
+++ metasploit.sh
@@ -5,10 +5,10 @@ find $HOME -name "metasploit*" -type d -exec rm -rf {} \;
 
 
 cwd=$(pwd)
-msfvar=4.17.40
+msfvar=5.0.83
 msfpath='/data/data/com.termux/files/home'
 
-apt update
+apt update && apt upgrade
 apt install -y libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make ruby libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner
 # Many phones are claiming libxml2 not found error
 ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
@@ -19,48 +19,46 @@ curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
 tar -xf $msfpath/$msfvar.tar.gz
 mv $msfpath/metasploit-framework-$msfvar $msfpath/metasploit-framework
 cd $msfpath/metasploit-framework
-sed '/rbnacl/d' -i Gemfile.lock
-sed '/rbnacl/d' -i metasploit-framework.gemspec
-gem install bundler
 
-#isNokogiri=$(gem list nokogiri -i)
+# Update rubygems-update
+if [ "$(gem list -i rubygems-update 2>/dev/null)" = "false" ]; then
+	gem install --no-document --verbose rubygems-update
+fi
 
-#sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+# Update rubygems
+update_rubygems
 
-#if [ $isNokogiri == "false" ];
-#then
-#      gem install nokogiri -v '1.8.0' -- --use-system-libraries
-#else
-#	echo "nokogiri already installed"
-#fi
+# Install bundler
+gem install --no-document --verbose bundler:1.17.3
 
-cd $msfpath/metasploit-framework && rm Gemfile.lock
-curl -LO https://hax4us.github.io/files/Gemfile.lock
-bundle update --bundler
-gem update --system
-bundle install -j5
-
+# Installing all gems 
+bundle config build.nokogiri --use-system-libraries
+bundle install -j3
 echo "Gems installed"
-$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
-rm ./modules/auxiliary/gather/http_pdf_authors.rb
 
-#ln -sf $msfpath/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
-#ln -sf $msfpath/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+# Some fixes
+sed -i "s@/etc/resolv.conf@$PREFIX/etc/resolv.conf@g" $msfpath/metasploit-framework/lib/net/dns/resolver.rb
+find "$msfpath"/metasploit-framework -type f -executable -print0 | xargs -0 -r termux-fix-shebang
+find "$PREFIX"/lib/ruby/gems -type f -iname \*.so -print0 | xargs -0 -r termux-elf-cleaner
 
-termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.6.0/gems/pg-0.20.0/lib/pg_ext.so
 echo "Creating database"
 
-cd $msfpath/metasploit-framework/config
-
+mkdir -p $msfpath/metasploit-framework/config && cd $msfpath/metasploit-framework/config
 curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/database.yml
 
-
 mkdir -p $PREFIX/var/lib/postgresql
-initdb $PREFIX/var/lib/postgresql
+pg_ctl -D "$PREFIX"/var/lib/postgresql stop > /dev/null 2>&1 || true
 
-pg_ctl -D $PREFIX/var/lib/postgresql start
-createuser msf
-createdb msf_database
+if ! pg_ctl -D "$PREFIX"/var/lib/postgresql start --silent; then
+    initdb "$PREFIX"/var/lib/postgresql
+    pg_ctl -D "$PREFIX"/var/lib/postgresql start --silent
+fi
+if [ -z "$(psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='msf'")" ]; then
+    createuser msf
+fi
+if [ -z "$(psql -l | grep msf_database)" ]; then
+    createdb msf_database
+fi
 
 rm $msfpath/$msfvar.tar.gz
 
