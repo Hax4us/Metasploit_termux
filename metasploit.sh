1ca579572fbe27abbbd268e3c0503cd7f0b64886
Lokesh Pandey <lkpandey950@gmail.com>
2021-02-13 18:59:36 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -9,7 +9,7 @@ msfvar=6.0.27
 msfpath='/data/data/com.termux/files/home'
 
 apt update && apt upgrade
-apt install -y libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make ruby libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner
+apt install -y libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make ruby2 libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner
 # Many phones are claiming libxml2 not found error
 ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
 
